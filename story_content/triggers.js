293bd0f5665b0ae454ac91a347b13892836b0345
Equipe-pedagogique <v.tarnai@manitou-group.com>
2026-02-17 13:07:36 +0100

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,35 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6L9YwWwg9ov":
+        Script1();
+        break;
+      case "6nhXPrTtliD":
+        Script2();
+        break;
+      case "61tZwyx3AUs":
+        Script3();
+        break;
+      case "6Nfup12Nz8f":
+        Script4();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
